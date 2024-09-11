c6797f2ef0f77df7a3e8f6a273c26cea521b1d1c
vishal kumar <uk706120@gmail.com>
2024-09-11 16:15:44 +0530

    multer

---
+++ validated-jwt-file-authentication - Copy/model/useSchema.js
@@ -0,0 +1,21 @@
+const {mongoose}=require('mongoose')
+const userSchema=new mongoose.Schema({
+    first_name:{
+        type:String,
+        required:true
+    },
+    last_name:{
+        type:String,
+        required:true
+    },
+    age:{
+        type:Number,
+        required:true
+     }
+},
+{timeStamps:true}
+)
+const user=mongoose.model("user",userSchema)
+module.exports={
+    user
+}
---
+++ validated-jwt-file-authentication - Copy/routes/user.js
@@ -0,0 +1,13 @@
+const {handleSignUp,handleLogin,validateLogin,validateSignUp,upload} =require("../controller/user")
+const express=require('express')
+
+// const {restrictToLoggedInUserOnly}=require("../middleware/auth")
+const router=express.Router()
+router.post('/signup',upload.single('uploadFile'),validateSignUp,handleSignUp)
+router.post('/login',validateLogin,handleLogin)
+
+
+module.exports={
+    signUpRouter:router,
+    
+}
